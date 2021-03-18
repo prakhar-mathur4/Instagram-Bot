fb73e73f44f5f681d5f262280825ac9cbc5994dc
Prakhar Mathur <63247633+prakhar-mathur4@users.noreply.github.com>
2021-03-18 12:24:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username="", password="")
+
+######  upload a picture #######
+bot.upload_photo("abc.jpg", caption="Caption Here")
+
+######  follow someone #######
+bot.follow("elonrmuskk")
+
+######  send a message #######
+bot.send_message("Hello from Instagram", ['user1','user2'])
+
+######  get follower info #######
+my_followers = bot.get_user_followers("xyz")
+
+for follower in my_followers:
+    print(follower)
+
+
+#### Unfollow All the Followers ####
+bot.unfollow_everyone()
